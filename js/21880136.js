974ab55e49331cdbda96f68df7b2f111a6e59118
Minh Thao <minhthao5648@gmail.com>
2022-10-06 23:00:25 +0700

    feature: add pagination

--- js/21880136.js
+++ js/21880136.js
@@ -1,13 +1,23 @@
 var url = "https://web1-api.herokuapp.com/api/";
 
-async function fetchDataAndBindingView(req, templateId, viewId) {
+async function fetchDataAndBindingView(
+  req,
+  templateId,
+  viewId,
+  paginationData
+) {
   var view = document.getElementById(viewId);
 
   var resp = await fetch(`${url}${req}`);
   var data = await resp.json();
-
   var source = document.getElementById(templateId).innerHTML;
   var template = Handlebars.compile(source);
-  var context = { data };
+  var context = paginationData ? data : { data };
+  if (paginationData) {
+    const array = req.split("?");
+    const pageString = array[1].split("=");
+    const currentPage = parseInt(pageString[1]);
+    context.currentPage = currentPage;
+  }
   view.innerHTML = template(context);
 }
